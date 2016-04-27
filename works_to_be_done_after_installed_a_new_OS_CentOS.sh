2d4d00d4c726f37fa3a12cd29e9c0f43866546b7
Zhao <zhjwpku@gmail.com>
2016-04-27 04:15:11 -0400

    Works to be done after a new CentOS been installed.
    Signed-off-by: Zhao <zhjwpku@gmail.com>

---
+++ works_to_be_done_after_installed_a_new_OS_CentOS.sh
@@ -0,0 +1,20 @@
+# close the fucking noise
+echo "blacklist pcspkr" > /etc/modprobe.d/blacklist.conf
+
+# update the system
+yum update -y
+
+# install vim
+yum install -y vim
+
+# install Extra Packages for Enterprise Linux
+yum install -y epel-release
+
+# install Development Tools
+yum groupinstall -y 'Development Tools'
+
+# git configurations
+git config --global user.email zhjwpku@gmail.com
+git config --global user.name Zhao Junwang
+git config --global core.editor vim
+git config --global credential.helper store
