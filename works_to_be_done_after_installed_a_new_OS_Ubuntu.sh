a3e050e391bb7f1211d78d49e4994b5cd9b44ac6
Zhao Junwang <zhjwpku@gmail.com>
2016-07-16 15:47:40 +0800

    works to be done after installed a new OS Ubuntu
    Signed-off-by: Zhao Junwang <zhjwpku@gmail.com>

---
+++ works_to_be_done_after_installed_a_new_OS_Ubuntu.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# update the system
+apt-get update -y
+apt-get upgrade -y
+
+# install vim
+apt-get install vim -y
+
+# install Chromium
+apt-get install chromium-browser -y
+
+# install sogou pinyin
+#echo "deb http://archive.ubuntukylin.com:10006/ubuntukylin trusty main" > /etc/apt/sources.list.d/ubuntukylin.list
+#apt-get update -y
+#apt-get install sogoupinyin -y
+
+# git configuartions
+git config --global user.email zhjwpku@gmail.com
+git config --global user.name "Zhao Junwang"
+git config --global core.editor vim
+git config --global credential.helper store
+git config --global push.default simple
+
+# remove libreoffice
+apt-get remove libreoffice-common -y
+
+# remove Amazon
+apt-get remove unity-webapps-common -y
+
+# autoremove
+apt autoremove -y
